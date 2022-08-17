6491683fdc93ada8e8ae78a2a5a74654ce0b2fb1
pablo <pdmiranda92@gmail.com>
2022-08-17 19:32:51 -0300

    cambio

--- src/components/loginform.js
+++ src/components/loginform.js
@@ -3,20 +3,15 @@ import "./loginform.css"
 
 const LoginForm = () => {
 
-    const [popupStyle, showPopup] = useState("hide")
 
-    const popup = () => {
-        showPopup("login-popup")
-        setTimeout(() => showPopup("hide"), 3000)
-    }
 
     return (
         <div className="cover">
-            <h1>LateNola</h1>
+            <h1>FIGUSWAP</h1>
             <input type="text" placeholder="username" />
             <input type="password" placeholder="password" />
 
-            <div className="login-btn" onClick={popup}>Login</div>
+            <div className="login-btn" >Login</div>
 
             <p className="text">Or login using</p>
 
@@ -27,10 +22,6 @@ const LoginForm = () => {
 
             <div className="signup">Not registered? Signup</div>
 
-            <div className={popupStyle}>
-                <h3>Login Failed</h3>
-                <p>Username or password incorrect</p>
-            </div>
 
         </div>
     )
